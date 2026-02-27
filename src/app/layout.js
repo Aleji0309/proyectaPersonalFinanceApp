653955e27019b001ff684b8d210c7bcc7f187440
Ale <aleandro@icsecurity.com>
2026-02-27 16:09:42 -0600

    Styeles added to sidebar

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,12 +1,18 @@
 import { Sidebar } from "@/components/layout/Sidebar";
 import {MainContainer} from "@/components/layout/MainContainer";
+import { Inter } from "next/font/google";
 import "./globals.css";
+
+const inter = Inter({
+  subsets: ["latin"],
+  variable: "--font-sans",
+});
 
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>
+      <body className={inter.variable}>
          {/* Contenedor de la app */}
           <div className="root-container grid grid-cols-3 gap-4">
           {/* Contenedor del Sidebar */}
--- src/components/layout/Sidebar.jsx
+++ src/components/layout/Sidebar.jsx
@@ -1,16 +1,102 @@
 import Link from "next/link";
 import React from "react";
+import {
+  House,
+  FolderSync,
+  BadgeDollarSign,
+  PiggyBank,
+  BanknoteX,
+} from "lucide-react";
 
 export const Sidebar = () => {
   return (
-    <div className="bg-amber-900 ">
-      Finance
+    <div className="sidebar-container bg-(--bg-sidebar) text-(--text-secondary) min-h-screen">
+      <h2 className="text-2xl text-white">finance</h2>
       <div className="link-container grid grid-flow-col grid-rows-6 gap-4 mt-8">
-        <Link href="/overview">Overview</Link>
-        <Link href="/transactions">Transactions</Link>
-        <Link href="/budgets">Budgets</Link>
-        <Link href="/pots">Pots</Link>
-        <Link href="/recurringBills">Recurring Bills</Link>
+        <Link
+          href="/overview"
+          className="group flex items-center gap-3 rounded-xs px-3 py-2
+                    text-(--text-secondary)
+                    hover:bg-white hover:text-black
+                    transition-colors"
+        >
+          <House
+            size={20}
+            className="
+                    text-(--text-secondary)
+                    group-hover:text-(--accent-hover)
+                    transition-colors"
+          />
+          <span>Overview</span>
+        </Link>
+
+        <Link
+          href="/transactions"
+          className="group flex items-center gap-3 rounded-xs px-3 py-2
+                      text-(--text-secondary)
+                    hover:bg-white hover:text-black
+                      transition-colors"
+        >
+          <FolderSync
+            size={20}
+            className="
+                        text-(--text-secondary)
+                        group-hover:text-(--accent-hover)
+                        transition-colors"
+          />
+          <span>Transactions</span>
+        </Link>
+
+        <Link
+          href="/budgets"
+          className="group flex items-center gap-3 rounded-xs px-3 py-2
+                    text-(--text-secondary)
+                  hover:bg-white hover:text-black
+                    transition-colors"
+        >
+          <BadgeDollarSign
+            size={20}
+            className="
+                    text-(--text-secondary)
+                    group-hover:text-(--accent-hover)
+                    transition-colors"
+          />
+          <span>Budgets</span>
+        </Link>
+
+        <Link
+          href="/pots"
+          className="group flex items-center gap-3 rounded-xs px-3 py-2
+                    text-(--text-secondary)
+                    hover:bg-white hover:text-black
+                    transition-colors"
+        >
+          <PiggyBank
+            size={20}
+            className="
+                  text-(--text-secondary)
+                  group-hover:text-(--accent-hover)
+                  transition-colors"
+          />
+          <span>Savings</span>
+        </Link>
+
+        <Link
+          href="/recurringBills"
+          className="group flex items-center gap-3 rounded-xs px-3 py-2
+                    text-(--text-secondary)
+                  hover:bg-white hover:text-black
+                    transition-colors"
+        >
+          <BanknoteX
+            size={20}
+            className="
+                  text-(--text-secondary)
+                  group-hover:text-(--accent-hover)
+                  transition-colors"
+          />
+          <span>Recurring Bills</span>
+        </Link>
       </div>
     </div>
   );
