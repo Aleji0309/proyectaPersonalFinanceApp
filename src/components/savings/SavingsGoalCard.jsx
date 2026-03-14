ded8e3862efca739f8a9ed22f3672a23aeeef100
Ale <aleandro@icsecurity.com>
2026-03-14 12:44:15 -0600

    SavingGoalCard component updated@ test

--- src/components/savings/SavingsGoalCard.jsx
+++ src/components/savings/SavingsGoalCard.jsx
@@ -1,18 +1,22 @@
-import React from 'react'
+import React from "react";
 
 export const SavingsGoalCard = ({ data }) => {
-  return ( <>
-    <div>
+  return (
+    <>
+      <div>
         <p>test</p>
-        { data.map( (dataSaving) =>  {
-            return ( 
+        {data.map((dataSaving) => {
+          return (
             <div key={dataSaving}>
-                {dataSaving.name}
-            </div>
-            )
-        })}
-    </div>
+                <div className="data-saving-container">
+              <div className="base-circle"> {dataSaving.name}</div>
+              <div className="saving-progress"></div>
 
+                </div>
+            </div>
+          );
+        })}
+      </div>
     </>
-  )
-}
+  );
+};
